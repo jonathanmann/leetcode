a218bc96a0a23ff53ceec88d6b8e56e569675434
Jonathan Mann <mann17@gmail.com>
2015-06-09 23:23:40 -0400

    add zigzag

---
+++ zigZigConversion.py
@@ -0,0 +1,20 @@
+class Solution:	
+	def convert(self,s,numRows):
+		if numRows == 1 or numRows >= len(s): return s
+		final = [[] for row in xrange(numRows)]
+		curr = 0
+		for i in xrange(len(s)):
+			if curr == numRows - 1: d = 0
+			if curr == 0: d = 1
+			print  numRows -1 - abs(numRows - 1 - i % (2 * numRows - 2)),curr
+
+			#final[numRows -1 - abs(numRows - 1 - i % (2 * numRows - 2))].append(s[i])
+			final[curr].append(s[i])
+			if d == 1:curr += 1
+			if d == 0:curr -= 1
+		return "".join(["".join(final[i]) for i in xrange(numRows)])
+
+s = "PAYPALISHIRING"
+sol = Solution()
+print sol.convert(s,3)
+
