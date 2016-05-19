acabcf0c4b9191c67a7ba9412251e8fd2d9c3289
Jonathan Mann <mann17@gmail.com>
2016-05-19 08:12:14 -0400

    add intersection solution

---
+++ intersection.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        s1 = set(nums1)
+        s2 = set(nums2)
+        x = []
+        for e in s1:
+            if e in s2:
+                x.append(e)
+        return x
